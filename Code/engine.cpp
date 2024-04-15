2b9bdb7f0022edd43620873d50d89f380a8670dc
MrIllas <44169046+MrIllas@users.noreply.github.com>
2024-04-15 17:14:00 +0200

    Up to date, although not yet

--- Code/engine.cpp
+++ Code/engine.cpp
@@ -225,15 +225,21 @@ void Init(App* app)
     const Program& texturedMeshProgram = app->programs[app->texturedMeshProgramIdx];
     app->texturedMeshProgram_uTexture = glGetUniformLocation(texturedMeshProgram.handle, "uTexture");
     u32 PatrickModelIndex = ModelLoader::LoadModel(app, "Patrick/Patrick.obj");
+    u32 GroundModelIndex = ModelLoader::LoadModel(app, "./ground.obj");
 
     glGetIntegerv(GL_MAX_UNIFORM_BLOCK_SIZE, &app->maxUniformBufferSize);
     glGetIntegerv(GL_UNIFORM_BUFFER_OFFSET_ALIGNMENT, &app->uniformBlockAlignment);
 
     app->localUnfiromBuffer = CreateConstantBuffer(app->maxUniformBufferSize);
 
-    app->entities.push_back({glm::identity<glm::mat4>(), PatrickModelIndex, 0, 0});
-    app->entities.push_back({ glm::identity<glm::mat4>(), PatrickModelIndex, 0, 0 });
-    app->entities.push_back({ glm::identity<glm::mat4>(), PatrickModelIndex, 0, 0 });
+    app->entities.push_back({ TransformPositionScale(vec3(5.0, 1.0, 0.0), vec3(1.0, 1.0, 1.0)), PatrickModelIndex, 0, 0});
+    app->entities.push_back({ TransformPositionScale(vec3(0.0, 1.0, 5.0), vec3(1.0, 1.0, 1.0)), PatrickModelIndex, 0, 0 });
+    app->entities.push_back({ TransformPositionScale(vec3(-5.0, 1.0, 0.0), vec3(1.0, 1.0, 1.0)), PatrickModelIndex, 0, 0 });
+
+    app->entities.push_back({ TransformPositionScale(vec3(0.0, -5.0, 0.0), vec3(1.0, 1.0, 1.0)), GroundModelIndex, 0, 0 });
+
+    app->lights.push_back({ LightType::LightType_Directional, vec3(1.0, 1.0, 1.0), vec3(1.0, -1.0, 1.0), vec3(0.0, 0.0, 0.0) });
+    app->lights.push_back({ LightType::LightType_Point, vec3(0.0, 1.0, 0.0), vec3(1.0, 1.0, 1.0), vec3(0.0, 0.0, 0.0) });
 
     app->ConfigureFrameBuffer(app->defferedFrameBuffer);
 
--- WorkingDir/base_model.glsl
+++ WorkingDir/base_model.glsl
@@ -34,13 +34,77 @@ void main()
 
 #elif defined(FRAGMENT) ///////////////////////////////////////////////
 
+layout(binding = 0, std140) uniform GlobalParams
+{
+	vec3 uCameraPosition;
+	uint uLightCount;
+	Light uLight[16];
+};
+
 in vec2 vTexCoord;
+in vec3 vPosition;
+in vec3 vNormal;
+in vec3 vViewDir;
+
 uniform sampler2D uTexture;
+
 layout(location = 0) out vec4 oColor;
+
+void CalculateBlitVars(in Light light, out vec3 ambient, out vec3 diffuse, out vec3 specular)
+{
+	vec3 lightDir = normalize(light.direction);
+
+	float ambientStrength = 0.2;
+	ambient = ambientStrength * light.color;
+
+	float diff = max(dot(vNormal, lightDir), 0.0f);
+	diffuse = diff * light.color;
+
+	float specularStrength = 0.1f;
+	vec3 reflectDir = reflect(-lightDir, vNormal);
+	vec3 normalViewDir = normalize(vViewDir);
+	float spec = pow(max(dot(normalViewDir, reflectDir), 0.0f), 32);
+	specular = specularStrength * spec * light.color;
+}
 
 void main()
 {
-	oColor = texture(uTexture, vTexCoord);
+	//oColor = texture(uTexture, vTexCoord);
+
+	vec4 textureColor = texture(uTexture, vTexCoord);
+	vec4 finalColor = vec4(0.0f);
+	for (int i = 0; i < uLightCount; ++i)
+	{
+		vec3 lightResult = vec3(0.0f);
+
+		vec3 ambient = vec3(0.0f);
+		vec3 diffuse = vec3(0.0f);
+		vec3 specular = vec3(0.0f);
+
+		if (uLight[i].type == 0)
+		{
+			Light light = uLight[i];
+
+			CalculateBlitVars(light, ambient, diffuse, specular);
+
+			lightResult = ambient + diffuse + specular;
+			finalColor += vec4(lightResult, 1.0) * textureColor;
+		}
+		else
+		{
+			Light light = uLight[i];
+
+			float constant = 1.0f;
+			float linear = 0.09f;
+			float quadratic = 0.032f;
+			float distance = length(light.position - vPosition);
+			float attenuation = 1.0f / (constant + linear * distance + quadratic * (distance*distance));
+
+			CalculateBlitVars(light, ambient, diffuse, specular);
+			lightResult = (ambient * attenuation) + (diffuse * attenuation) + (specular * attenuation);
+			finalColor += vec4(lightResult, 1.0) * textureColor;
+		}
+	}
 }
 
 #endif
